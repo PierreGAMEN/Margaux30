dfbecc87a797fd772afc55e69ea8d54d78c3b27e
Pierre GAMEN <136753701+PierreGAMEN@users.noreply.github.com>
2026-04-28 22:54:47 +0200

    Update gifts.js

--- src/data/gifts.js
+++ src/data/gifts.js
@@ -103,6 +103,6 @@ export const totalPrice = gifts.reduce((sum, g) => sum + g.price, 0)
 
 export const cagnotte = {
   objectif: totalPrice,
-  collecte: 510, // ← tu mets à jour ici au fur et à mesure
+  collecte: 560, // ← tu mets à jour ici au fur et à mesure
 }
 
